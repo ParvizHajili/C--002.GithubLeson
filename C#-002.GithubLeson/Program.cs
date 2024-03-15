09ec79ca93f5d20a61b28b104da8b04b69eb9e62
ParvizHajili <parviz.hajili@gmail.com>
2024-03-15 16:06:03 +0400

    Add project files.

---
+++ C#-002.GithubLeson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__002.GithubLeson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
